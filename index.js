bf9b0055a956e51d8e429d60177d1c6b4f4e7a38
Glitch (wealthy-laced-margin) <none>
2021-03-10 09:10:38 +0000

    Import of FriskaNaila/DraconianJSBot

--- index.js
+++ index.js
@@ -1,77 +1,132 @@
-const { Client, Collection, MessageEmbed, Guild } = require("discord.js")
+require('dotenv').config()
 
-const fs = require("fs")
-const client = new Client({
-  disableEveryone: "true" // This makes sure that the bot does not mention everyone
+const Enmap = require('enmap');
+const Discord = require('discord.js');
+const client = new Discord.Client({
+	disableMentions: 'everyone'
 });
-client.commands = new Collection();
-client.aliases = new Collection();
-client.categories = fs.readdirSync("./Commands/")
-const config = require("./config.json")// enter your bot prefix in the config.json file
-const mongoose = require("mongoose")
-const prefixModel = require("./database/guildData/prefix")
+const config = require('./config.json');
+const fs = require('fs');
+client.config = config;
+client.login(process.env.TOKEN);
+const db = require('quick.db');
+// Delete 2 lines code if you don't use discord bot lists of top.gg
+// const DBL = require('dblapi.js');
+// const dbl = new DBL(process.env.DBLTOKEN, client);
+const { GiveawaysManager } = require('discord-giveaways');
+client.commands = new Discord.Collection();
+client.aliases = new Discord.Collection();
+client.emotes = config.emoji;
+client.colors = client.config.colors;
+client.snipes = new Map();
+// client.dbl = dbl
+const cooldowns = new Discord.Collection();
+const settings = require('./settings.json');
+let nz_date_string = new Date().toLocaleString("en-US", { timeZone: "Asia/Hong_Kong" });
+client.mapss = new Map();
+client.mapss.set('uptimedate', nz_date_string);
+["command", "event", "giveaway", "music"].forEach(x => require(`./handlers/${x}.js`)(client));
+// ["dbl-loader"].forEach(x => require(`./handlers/${x}.js`)(client));
+const httpOutPut = require('./server/http');
+const alwaysOn = require('./server/alwaysOn');
 
-['command'].forEach(handler => {
-  require(`./handler/${handler}`)(client);
-})
+httpOutPut()
+alwaysOn()
 
-mongoose.connect(config.mongoPass, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useFindAndModify: false,
-});
+// Delete this area of code if you don't use discord bot lists
+// const blapi = require('blapi');
+// const apiKeys = {
+// 	'botlist-url1': process.env.btoken1, // Add other bot lists as needed
+// 	'botlist-url2': process.env.btoken2
+// };
+// blapi.handle(client, apiKeys, 60);
+// blapi.setLogging(true);
 
-client.on("ready", ()=> {
-  client.user.setPresence({ status: 'online' });
-  client.user.setActivity("Hello", {type: "STREAMING"});
-  console.log(`Logged in as ${client.user.tag}`)
-})
-
-client.on('message', async (message) => {
-  const data = prefixModel.findOne({ GuildID: message.guild.id })
-  if (data) {
-   var prefix = data.Prefix;
-  } else if (!data) {
-   prefix = config.DEFAULT_PREFIX;
-  }
-client.prefix = prefix;
-
-    if (message.author.bot) return; // This line makes sure that the bot does not respond to other bots
-    if (!message.guild) return;
-    if (!message.content.startsWith(prefix)) return; // This line makes sure that the bot does not respond to other messages with the bots prefix
-    if (!message.member) message.member = await message.guild.fetchMember(message);
-
-    const args = message.content.slice(prefix.length).trim().split(/ +/g);
-    const cmd = args.shift().toLowerCase();
-    if (cmd.length === 0) return;
-
-    let command = client.commands.get(cmd);
-    if (!command) command = client.commands.get(client.aliases.get(cmd));
-    if (command)
-      command.run(client, message, args);
-
-}
+client.settings = new Enmap({
+	name: 'settings',
+	fetchAll: false,
+	autoFetch: true,
+	cloneLevel: 'deep'
 });
 
-// Auto-Role is here!
-
-client.on("guildMemberAdd", async(member)=>{
-  const roleData = require("./database/guildData/autorole")
-  const data = await roleData.findOne({
-      GuildID: member.guild.id,
-  }).catch(err=>console.log(err));
-  
-  if (data) {
-      let role = data.Role;
-      let arole = member.guild.roles.cache.get(role);
-    if (role) {
-      member.roles.add(arole)
-    } else if (!role) {
-        return;
-    }
-  } else if (!data) {
-      return;
-  }
+const DisTube = require('distube');
+client.distube = new DisTube(client, {
+	leaveOnFinish: true,
+	leaveOnEmpty: true,
+	leaveOnStop: true,
+	youtubeDL: true,
+	updateYouTubeDL: true,
+	youtubeCookie: "GPS=1; YSC=w5dGoHzqQRI; VISITOR_INFO1_LIVE=B4ElBqxSDv4; PREF=tz=Asia.Hong_Kong"
 });
+if (!db.get('giveaways')) db.set('giveaways', []);
 
-client.login("YOUR_TOP_SECRET_BOT_TOKEN!")//Enter your bot token here
+const GiveawayManagerWithOwnDatabase = class extends GiveawaysManager {
+	// This function is called when the manager needs to get all the giveaway stored in the database.
+	async getAllGiveaways() {
+		// Get all the giveaway in the database
+		return db.get('giveaways');
+	}
+
+	// This function is called when a giveaway needs to be saved in the database (when a giveaway is created or when a giveaway is edited).
+	async saveGiveaway(messageID, giveawayData) {
+		// Add the new one
+		db.push('giveaways', giveawayData);
+		// Don't forget to return something!
+		return true;
+	}
+
+	async editGiveaway(messageID, giveawayData) {
+		// Gets all the current giveaways
+		const giveaways = db.get('giveaways');
+		// Remove the old giveaway from the current giveaways ID
+		const newGiveawaysArray = giveaways.filter(
+			giveaway => giveaway.messageID !== messageID
+		);
+		// Push the new giveaway to the array
+		newGiveawaysArray.push(giveawayData);
+		// Save the updated array
+		db.set('giveaways', newGiveawaysArray);
+		// Don't forget to return something!
+		return true;
+	}
+
+	// This function is called when a giveaway needs to be deleted from the database.
+	async deleteGiveaway(messageID) {
+		// Remove the giveaway from the array
+		const newGiveawaysArray = db
+			.get('giveaways')
+			.filter(giveaway => giveaway.messageID !== messageID);
+		// Save the updated array
+		db.set('giveaways', newGiveawaysArray);
+		// Don't forget to return something!
+		return true;
+	}
+};
+
+// Create a new instance of your new class
+const manager = new GiveawayManagerWithOwnDatabase(client, {
+	storage: false, // Important - use false instead of a storage path
+	updateCountdownEvery: 10000,
+	endedGiveawaysLifetime: 30000,
+	hasGuildMembersIntent: false,
+	default: {
+		botsCanWin: false,
+		exemptPermissions: ['MANAGE_MESSAGES', 'ADMINISTRATOR'],
+		embedColor: '#ff6969',
+		embedColorEnd: "#505050",
+		reaction: '🎉'
+	}
+});
+// We now have a giveawaysManager property to access the manager everywhere!
+client.giveawaysManager = manager;
+// We now have a client.giveawaysManager property to manage our giveaways!
+
+client.status = queue =>
+	`Volume: \`${queue.volume}%\` | Filter: \`${queue.filter ||
+	'Off'}\` | Loop: \`${
+	queue.repeatMode
+		? queue.repeatMode == 2
+			? 'All Queue'
+			: 'This Song'
+		: 'Off'
+	}\` | Autoplay: \`${queue.autoplay ? 'On' : 'Off'}\``;
